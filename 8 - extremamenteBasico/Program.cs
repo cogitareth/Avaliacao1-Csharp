4dcc037b3705b6c43ccb6bfc5e406489fce25a93
Gabriel Henrique <turles41@gmail.com>
2023-04-14 20:38:07 -0300

    add 8

---
+++ 8 - extremamenteBasico/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int resultado = a + b;
+
+        Console.WriteLine("X =" + resultado);
+    }
+}
