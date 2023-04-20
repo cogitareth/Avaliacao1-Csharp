adfcda37e4fb859d0c8de6f60689dae999a8aced
Gabriel Henrique <turles41@gmail.com>
2023-04-20 11:49:43 -0300

    ultimas alterações

--- z24 - felizNatal/Program.cs
+++ z24 - felizNatal/Program.cs
@@ -8,7 +8,7 @@ class Program
         int I = Convert.ToInt32(Console.ReadLine());
 
         // Monta a frase "Feliz natal!" com a última letra 'a' repetida I vezes
-        string frase = "Feliz natal" + new string('a', I) + "l!";
+        string frase = "Feliz nata" + new string('a', I) + "l!";
 
         // Imprime a frase com quebra de linha
         Console.WriteLine(frase);
